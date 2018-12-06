c0ba86b0c689a6f99fd6cc10d49f1ccc0d5010c7
Pavel Aparcana <gpaparcana@gmail.com>
2018-12-06 12:03:39 -0500

    fixed and updaed
    fixed bug and wrote more scripts for sprites. Added the GUI

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -2,20 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dray : MonoBehaviour, IFacingMover 
+public class Dray : MonoBehaviour, IFacingMover, IKeyMaster 
 {
     public enum eMode { idle, move, attack, transition }
     [Header("Set in Inspector")]
     public float speed = 5;
     public float attackDuration = 0.25f;
     public float attackDelay = 0.5f;
+    public float transitionDelay = 0.5f;
+    public int maxHealth = 10;
 
     [Header("Set Dynamically")]
     public int dirHeld = -1;
     public int facing = 1;
     public eMode mode = eMode.idle;
+    public int numKeys = 0;
+
+    [SerializeField]
+    private int _health;
+    public int health
+    {
+        get { return _health; }
+        set { _health = value; }
+    }
     private float timeAtkDone = 0;
     private float timeAtkNext = 0;
+    private float transitionDone = 0;
+    private Vector2 transitionPos;
     private Rigidbody rigid;
     private Animator anim;
     private InRoom inRm;
@@ -31,10 +44,20 @@ public class Dray : MonoBehaviour, IFacingMover
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         inRm = GetComponent<InRoom>();
+        health = maxHealth;
     }
 
     void Update()
     {
+        if (mode == eMode.transition)
+        { 
+            rigid.velocity = Vector3.zero;
+            anim.speed = 0;
+            roomPos = transitionPos;
+            if (Time.time < transitionDone) return;
+
+            mode = eMode.idle;
+        }
         dirHeld = -1;
         for (int i = 0; i < 4; i++)
         {
@@ -87,6 +110,49 @@ public class Dray : MonoBehaviour, IFacingMover
         rigid.velocity = vel * speed;
     }
 
+    void LateUpdate()
+    {
+        Vector2 rPos = GetRoomPosOnGrid(0.5f);
+        int doorNum;
+        for (doorNum = 0; doorNum < 4; doorNum++)
+        {
+            if (rPos == InRoom.DOORS[doorNum])
+            {
+                break;
+            }
+        }
+
+        if (doorNum > 3 || doorNum != facing) return;
+        Vector2 rm = roomNum;
+        switch (doorNum)
+        {
+            case 0:
+                rm.x += 1;
+                break;
+            case 1:
+                rm.y += 1;
+                break;
+            case 2:
+                rm.x -= 1;
+                break;
+            case 3:
+                rm.y -= 1;
+                break;
+        }
+
+        if (rm.x >= 0 && rm.x <= InRoom.MAX_RM_X)
+        {
+            if (rm.y >= 0 && rm.y <= InRoom.MAX_RM_Y)
+            {
+                roomNum = rm;
+                transitionPos = InRoom.DOORS[(doorNum + 2) % 4];
+                roomPos = transitionPos;
+                mode = eMode.transition;
+                transitionDone = Time.time + transitionDelay;
+            }
+        }
+    }
+
     // Implementation of IFacingMover
     public int GetFacing()
     {
@@ -126,5 +192,12 @@ public class Dray : MonoBehaviour, IFacingMover
     public Vector2 GetRoomPosOnGrid(float mult = -1)
     {
         return inRm.GetRoomPosOnGrid(mult);
+    }
+
+    // Implementation of IKeyMaster
+    public int keyCount
+    {
+        get { return numKeys; }
+        set { numKeys = value; }
     }
 }
